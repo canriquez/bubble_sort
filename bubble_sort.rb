5c9c414121e76236766693565661ac142f10edc5
Carlos Anriquez <canriquez@gmail.com>
2020-02-18 16:14:24 -0300

    rubocop corrections done..

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,15 @@
+def bubble_sort(arr)
+  counter = 1 # initialized to 1.
+  while counter <= arr.length - 1 # Check if we have run length-1 times the code
 
-def bubble_sort (arr)
- 
- counter =1   #initialized to 1.
-    while counter <= arr.length-1 do  #Check if we have run length-1 times the code
+    arr.each_with_index do |value, index| # using this method to have a self updated index done by the method
+      next unless (value <=> arr[index + 1]) == 1 # We use the combined comparison operator to find who is biggest
 
-        arr.each_with_index do |value,index|     #using this method to have a self updated index done by the method
-
-            if (value <=> arr[index+1]) == 1    # We use the combined comparison operator to find who is biggest
-
-                arr[index], arr[index+1] = arr[index+1], arr[index]    #we transpose the elements if left > right 
-
-            end
-        end
-        counter +=1           #increase counter
+      arr[index], arr[index + 1] = arr[index + 1], arr[index] # we transpose the elements if left > right
     end
-    return arr
+    counter += 1 # increase counter
+  end
+  arr
 end
 
-
-
-puts bubble_sort([4,25,3,243,3453,12,3,5,4])
-
+puts bubble_sort([4, 25, 3, 243, 3453, 12, 3, 5, 4])
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,18 +1,15 @@
+def bubble_sort_by(arr)
+  counter = 1 # initialized to 1.
+  while counter <= arr.length - 1 # Check if we have run length-1 times the code
+    arr.each_with_index do |_value, index| # using this method to have a self updated index done by the method
+      next unless index < arr.length - 1 # as long as the index is less than the length of the array
 
-def bubble_sort_by (arr)
- 
-    counter =1   #initialized to 1.
-       while counter <= arr.length-1 do  #Check if we have run length-1 times the code
-           arr.each_with_index do |value,index|  #using this method to have a self updated index done by the method
-                if index < arr.length-1 #as long as the index is less than the length of the array
-                    if yield(arr[index],arr[index+1])>0  #if the yield(block) evaluates to a number > than 0 : execute
-                        arr[index], arr[index+1] = arr[index+1], arr[index]
-                    end
-                end
-           end
-           counter +=1   #increase counter
-       end
-       return arr
-   end
-   
-puts bubble_sort_by(["hi","hello","hey"]) {|left,right| right.length - left.length}
+      arr[index], arr[index + 1] = arr[index + 1], arr[index] if yield(arr[index], arr[index + 1]).positive?
+      # if the yield(block) evaluates to a number > than 0 : execute
+    end
+    counter += 1 # increase counter
+  end
+  arr
+end
+
+puts bubble_sort_by(%w[hi hello hey]) { |left, right| right.length - left.length }
